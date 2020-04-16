4fa2a1d5c2b7c3be69cef7f4f4441b91b6dc19a1
Kartikey45 <singhkartikey45@gmail.com>
2020-04-16 15:59:27 +0530

    ADDED : Welcome to Indian State Census Analyser Problem

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser
+{
+    // Main class
+    public class Program
+    {
+        // Main method
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyser Problem");
+            Console.ReadKey();
+        }
+    }
+}
